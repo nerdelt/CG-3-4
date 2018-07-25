0956d29cf829a3a5af098bf2a9aac09296b6f764
nerdelt <nerdelt@gmail.com>
2018-07-25 15:33:55 -0500

    Assignment 3-4 2nd attempt; wrong repo at first

---
+++ SimpleCalc/SimpleCalc/SimpleCalc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace SimpleCalc
+{
+    class SimpleCalc
+    {
+        static void Main(string[] args)
+        {
+            //introduces program to user and asks for first number.
+            Console.WriteLine("Let's perform a simple calculation between two numbers. ");
+            Console.Write("Please enter the first number: ");
+
+            //turn first number into double 
+            string firstNumber = Console.ReadLine();
+            double first = double.Parse(firstNumber);
+
+            //ask for second number and turn it into double
+            Console.Write("Please enter the second number: ");
+            string secondNumber = Console.ReadLine();
+            double second = double.Parse(secondNumber);
+
+            //asks user for calculation method
+            Console.Write("Would you like to add, subtract, multiply, or divide? ");
+            string operation = Console.ReadLine();
+            double answer = 0;
+
+            //operation possibilites
+            if (operation == "add")
+            {
+                answer = first + second;
+                Console.WriteLine($"{first} + {second} = {answer}");
+            }
+
+            else if (operation == "subtract")
+            {
+                answer = first - second;
+                Console.WriteLine($"{first} - {second} = {answer}");
+            }
+
+            else if (operation == "multiply")
+            {
+                answer = first * second;
+                Console.WriteLine($"{first} x {second} = {answer}");
+            }
+
+            else if (operation == "divide")
+            {
+                answer = first / second;
+                Console.WriteLine($"{first} / {second} = {answer}");
+            }
+
+            else
+            {
+                Console.WriteLine("There was a problem. Please enter add, subtract, multiply, or divide.");
+
+            }
+
+            //allows user to see the answer
+            Console.WriteLine($"The answer is: {answer}");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
